8e49927f06bbbaf84aacffff69ee2ac469e3389d
Annu Poddar <annupoddar444@gmail.com>
2024-06-09 19:27:53 +0530

    backend app

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/api', (req, res) => {
+    res.send("Api route")
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`)
+})
